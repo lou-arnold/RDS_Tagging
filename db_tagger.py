55be788d9ee5f178313b66ad32dafacb7e5a5f45
Louis Arnold <83360511+lou-arnold@users.noreply.github.com>
2022-03-23 09:17:39 -0500

    Update db_tagger.py

--- db_tagger.py
+++ db_tagger.py
@@ -10,6 +10,8 @@ def get_db_arn():
     #database_arn = []
     for db in response_iterator:
         db_instances = db['DBInstances']
+        
+        '''Make this a func -- Finds all non tagged dbs -- then return the array of non-tagged'''
         for identifier in db_instances:
             database_arn = (identifier['DBInstanceArn'])
             database_tag = (identifier['TagList'])
@@ -17,8 +19,11 @@ def get_db_arn():
             
 get_db_arn()
 
+
 '''
+Then this func would go thrugh the array and tag 
 def id_tagging(database_arn):
     for each in x:
         client.list_tags_for_resource()
         '''
+
